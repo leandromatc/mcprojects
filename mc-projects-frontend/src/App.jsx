46a4b782350866950ea6d17e66d7f87952cb6bba
Leandro Matosas <lnmatosas@hotmail.com>
2023-09-30 20:36:55 -0300

    Page animation

--- mc-projects-frontend/src/App.jsx
+++ mc-projects-frontend/src/App.jsx
@@ -4,18 +4,22 @@ import Navbar from "./components/Navbar";
 import Clients from "./components/Clients";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="clients" element={<Clients />} />
-        <Route path="contact" element={<Contact />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes key={location.pathname} location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="clients" element={<Clients />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
---
+++ mc-projects-frontend/src/components/AnimatedPages.jsx
@@ -0,0 +1,21 @@
+import { motion } from "framer-motion";
+function AnimatedPages({ children }) {
+  const animations = {
+    initial: { opacity: 0, y: -100 },
+    animate: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: 100 },
+  };
+  return (
+    <motion.div
+      variants={animations}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition={{ duration: 0.3 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default AnimatedPages;
